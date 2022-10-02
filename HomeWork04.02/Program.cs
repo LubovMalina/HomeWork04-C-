f3d702407c72543cd31a7a0eb0943779eb9e5c69
LubovMalina <andreeva.lubov27@gmail.com>
2022-10-02 20:26:38 +0300

    Сумма чисел в числе

---
+++ HomeWork04.02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+while (number > 0)
+{
+    int last_number = number%10;
+    sum += last_number; 
+    number=number/10;
+
+}
+
+Console.WriteLine($"Сумма чисел введенного числа равна  - > {sum}");
