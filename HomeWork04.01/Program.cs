0edeaf91b1e9187f74a64ccddacc3b0113b2b633
LubovMalina <andreeva.lubov27@gmail.com>
2022-10-02 20:09:26 +0300

    Возведение числа в натуральную степень

---
+++ HomeWork04.01/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+*/
+Console.Clear();
+
+Console.WriteLine("Введите число A:");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B:");
+int numberB = int.Parse(Console.ReadLine());
+
+int step = 1;
+
+for (int i = 0; i < numberB; i++)
+{
+    step = step * numberA;
+}
+
+Console.WriteLine($"Введенное число {numberA} возведенное в натуральную степень {numberB} равно -> {step}");
+
