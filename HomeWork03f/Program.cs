31a385c0bce71b346ab1b4f90ec3747415fbdde0
LubovMalina <andreeva.lubov27@gmail.com>
2022-10-04 11:27:32 +0300

    Задача 3, один из вариантов

---
+++ HomeWork03f/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+Console.Clear();
+Console.WriteLine("Введите длину массива : ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("[ ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < arraySize; i++)
+    {
+        Console.WriteLine("Введите элемент массива: ");
+        int numbers = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(numbers + " ");
+    }
+//Console.Write(numbers + " ");
+Console.Write("]");
+
+
+/* int[] numbers = new int[arraySize];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Ваш массив: ");
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1,99);
+    }
+}
+
+*/
